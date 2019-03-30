c3e8ad6046b7c90a1091fff095e21efad50f704a
vagarioustoast <vagarioustoast@gmail.com>
2019-03-30 14:51:41 -0700

    makes author name required

--- models/Author.js
+++ models/Author.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true
+  },
   lifeSpan: String,
   biography: String,
   authorPicUrl: String
