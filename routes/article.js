3ce1d210fb830573f338b44b13ae18cc71f6f2d1
vagarioustoast <vagarioustoast@gmail.com>
2019-04-06 14:13:06 -0700

    edits id property for showOneArticle

--- routes/article.js
+++ routes/article.js
@@ -4,7 +4,7 @@ const controllers = require("../controllers");
 
 router.get("/all", controllers.article.index);
 router.get("/category/:category", controllers.article.showCategory);
-router.get("/articles/:id", controllers.article.showOneArticle);
+router.get("/article/:id", controllers.article.showOneArticle);
 router.post("/create", controllers.article.createArticle);
 router.put("/update", controllers.article.updateArticle);
 
